930925c165e8f260285c1004ad5d3c61522fa86f
Islam Bahnas <islambahnas1@gmail.com>
2022-07-12 23:46:18 +0200

    Update models, add relations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "users#index"
+
   resources :budgets
   resources :groups
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
