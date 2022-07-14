3b13d129bb057518697f0b4e7c64081063b13b92
Islam Bahnas <islambahnas1@gmail.com>
2022-07-14 02:28:29 +0200

    Add new group method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = Group.where(user: current_user)
   end
 
   # GET /groups/1 or /groups/1.json
@@ -11,10 +11,30 @@ class GroupsController < ApplicationController
   end
 
   # GET /groups/new
-  def new
-    @group = Group.new
-  end
 
   def create
+    add_group = current_user.group.new(group_params)
+    # @group = Group.new(group_params)
+
+    if add_group.save
+      # render json: @group, status: :created, location: @group
+      redirect_to groups_path, notice: 'Category added successfully'
+    else
+      # render json: @group.errors, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity, alert: 'Failed to add category'
+    end
+  end
+
+  private
+  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def group_params
+    # params.require(:group).permit(:name, :icon)
+    params.permit(:name, :icon)
   end
 end
