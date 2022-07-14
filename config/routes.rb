5ce04d8471ef01cf6dc4932ac8e181d8d5ef6a32
Islam Bahnas <islambahnas1@gmail.com>
2022-07-14 23:14:03 +0200

    Add welcome page, change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-
-  root to: "groups#index"
-
-  resources :users, only: [:index]
+  root 'splash#index'
+  resources :splash, only: [:index]
 
   resources :groups, only: [:index, :new, :create, :destroy ] do
     resources :payments, only: %i[index new create destroy update]
